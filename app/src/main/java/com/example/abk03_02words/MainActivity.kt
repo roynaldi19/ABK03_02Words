0be1e9160e858ef70f04630d930fe2c90ef7f64a
roynaldi19 <roynaldi19@gmail.com>
2021-07-04 09:02:25 +0700

    comit 1

--- app/src/main/java/com/example/abk03_02words/MainActivity.kt
+++ app/src/main/java/com/example/abk03_02words/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.abk03_02words
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.abk03_02words.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        // Sets the LinearLayoutManager of the recyclerview
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = LetterAdapter()
     }
 }
